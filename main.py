fdd1c70e9541948718e94074b5753d3351619ed9
sumityadav1509 <sumityadav2005@gmail.com>
2021-01-20 05:40:49 +0000

    Used multiple return values while defining a function.

--- main.py
+++ main.py
@@ -11,12 +11,32 @@ print(output)
 def format_name(f_name,l_name):
   f_capital=f_name.title()
   l_capital=l_name.title() 
+
   return (f"{f_capital} {l_capital}") 
+  
 
 
 formated_string=format_name("sumit","SUmiT")
+# used .title() function for re-arranging strings 
+print(formated_string) 
+
+# using multiple return values:
+
+def format_name(f_name,l_name):
   
+  if f_name=="" or l_name=="": 
+    return
+  
+  
+  f_capital=f_name.title()
+  l_capital=l_name.title() 
+
+  return (f"{f_capital} {l_capital}") 
+  
+
+
+formated_string=format_name(input("Waht is your first name ?"),input("What is your last name? "))
+# used .title() function for re-arranging strings 
 print(formated_string) 
 
 
-# format_name("sumit","SUMIT")
