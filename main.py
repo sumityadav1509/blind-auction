f85da45eae8a45573c4d10a5969092485e6211fc
sumityadav1509 <sumityadav2005@gmail.com>
2021-01-22 10:42:34 +0000

    Initial Debugging completed.

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ operations={
 
 } 
 num1=int(input("What's the first number?: "))
-num2=int(input("What's the second number?:")) 
+ 
 
 # function=operations["+"]
 # print(function) 
@@ -35,12 +35,15 @@ for symbol in operations:
   print(symbol)
 
 operation_symbol=input("Pick an operation from the line above: ") 
+num2=int(input("What's the second number?:")) 
 calculation_function=operations[operation_symbol] 
-first_answer= calculation_function(num1,num2) 
+first_answer= int(calculation_function(num1,num2) )
 
 print(f"{num1}{operation_symbol}{num2}={first_answer}")
+
+# Debugging required:
 operation_symbol=input("Pick another operation: ")
 num3= int(input("What's the next number ? "))
 calculation_function=operations[operation_symbol]
-second_answer=calculation_function(calculation_function(num1,num2) ,num3) 
+second_answer=calculation_function(first_answer ,num3) 
 print(f"{first_answer}{operation_symbol}{num3}={second_answer}")
