1ed13a078aad643e5f3dc520869086a05cf10ae9
sumityadav1509 <sumityadav2005@gmail.com>
2021-01-22 10:57:12 +0000

    Used a while loop and a if-else statement.Further improvement in user experience.

--- main.py
+++ main.py
@@ -33,17 +33,19 @@ num1=int(input("What's the first number?: "))
 # print(function) 
 for symbol in operations:
   print(symbol)
+should_continue=True 
 
-operation_symbol=input("Pick an operation from the line above: ") 
-num2=int(input("What's the second number?:")) 
-calculation_function=operations[operation_symbol] 
-first_answer= int(calculation_function(num1,num2) )
+while should_continue:
 
-print(f"{num1}{operation_symbol}{num2}={first_answer}")
+  operation_symbol=input("Pick an operation from the line above: ") 
+  num2=int(input("What's the second number?:")) 
+  calculation_function=operations[operation_symbol] 
+  answer= int(calculation_function(num1,num2) )
 
-# Debugging required:
-operation_symbol=input("Pick another operation: ")
-num3= int(input("What's the next number ? "))
-calculation_function=operations[operation_symbol]
-second_answer=calculation_function(first_answer ,num3) 
-print(f"{first_answer}{operation_symbol}{num3}={second_answer}")
+  print(f"{num1}{operation_symbol}{num2}={answer}")
+
+  if input(f"Type 'y' to continue calculating with {answer} , or type 'n' to exit : ")=="y":
+    num1=answer 
+  else:
+    should_continue=False 
+    
